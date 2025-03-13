af687d2caa1ac346005d1787cbc6825e801117de
kasyu04 <kasyu04@gmail.com>
2025-03-13 22:00:36 +0900

    first commit.

--- components.py
+++ components.py
@@ -61,7 +61,7 @@ def display_conversation_log():
                         icon = utils.get_source_icon(message['content']['main_file_path'])
                         # 参照元ドキュメントのページ番号が取得できた場合にのみ、ページ番号を表示
                         if "main_page_number" in message["content"]:
-                            st.success(f"{message['content']['main_file_path']} - ページNo.: {message['content']['main_page_number']}", icon=icon)
+                            st.success(f"{message['content']['main_file_path']} - (ページNo. {message['content']['main_page_number'] + 1})", icon=icon)
                         else:
                             st.success(f"{message['content']['main_file_path']}", icon=icon)
                         
@@ -78,7 +78,7 @@ def display_conversation_log():
                                 icon = utils.get_source_icon(sub_choice['source'])
                                 # 参照元ドキュメントのページ番号が取得できた場合にのみ、ページ番号を表示
                                 if "page_number" in sub_choice:
-                                    st.info(f"{sub_choice['source']} - ページNo.: {sub_choice['page_number']}", icon=icon)
+                                    st.info(f"{sub_choice['source']} - (ページNo. {sub_choice['page_number'] + 1})", icon=icon)
                                 else:
                                     st.info(f"{sub_choice['source']}", icon=icon)
                     # ファイルのありかの情報が取得できなかった場合、LLMからの回答のみ表示
@@ -134,7 +134,7 @@ def display_search_llm_response(llm_response):
             # ページ番号を取得
             main_page_number = llm_response["context"][0].metadata["page"]
             # 「メインドキュメントのファイルパス」と「ページ番号」を表示
-            st.success(f"{main_file_path} - ページNo.: {main_page_number}", icon=icon)
+            st.success(f"{main_file_path} - (ページNo. {main_page_number + 1})", icon=icon)
         else:
             # 「メインドキュメントのファイルパス」を表示
             st.success(f"{main_file_path}", icon=icon)
@@ -169,7 +169,7 @@ def display_search_llm_response(llm_response):
                 # ページ番号を取得
                 sub_page_number = document.metadata["page"]
                 # 「サブドキュメントのファイルパス」と「ページ番号」の辞書を作成
-                sub_choice = {"source": sub_file_path, "page_number": sub_page_number}
+                sub_choice = {"source": sub_file_path, "page_number": sub_page_number + 1}
             else:
                 # 「サブドキュメントのファイルパス」の辞書を作成
                 sub_choice = {"source": sub_file_path}
@@ -270,7 +270,7 @@ def display_contact_llm_response(llm_response):
                 # ページ番号を取得
                 page_number = document.metadata["page"]
                 # 「ファイルパス」と「ページ番号」
-                file_info = f"{file_path}"
+                file_info = f"{file_path} - (ページNo. {page_number + 1})"
             else:
                 # 「ファイルパス」のみ
                 file_info = f"{file_path}"
